668409acffc1ee154d4996e0aa8fd58448d95b8d
Кузнецов Михаил Владимирович <kuznetsov.m@russianpost.ru>
2022-05-07 21:53:12 +0300

    init2

--- src/main/java/ru/gb/jmsexample/sender/MailService.java
+++ src/main/java/ru/gb/jmsexample/sender/MailService.java
@@ -1,22 +1,9 @@
 package ru.gb.jmsexample.sender;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
 public class MailService {
-
-    private final JavaMailSender javaMailSender;
-
-    public void sendMail(String to, String subject, String msg) {
-        SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
-        simpleMailMessage.setFrom("kuznetsovmk@gmail.com");
-        simpleMailMessage.setTo(to);
-        simpleMailMessage.setSubject(subject);
-        simpleMailMessage.setText(msg);
-        javaMailSender.send(simpleMailMessage);
-    }
 }
